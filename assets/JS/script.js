83c15671c0c6debc041b5c2530c7f634838e90dd
BrunoBernardini <bruno.f.bernardini@gmail.com>
2022-05-11 16:18:44 +0200

    Completamento esercizio.

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -7,3 +7,31 @@ generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
 Bonus
 Far comparire gli indirizzi email solamente quando sono stati tutti generati.
  */
+
+const app = new Vue({
+  el: "#app",
+  data: {
+    emailList: [],
+    generated: false
+  },
+  methods: {
+    generateEmailList(quantity){
+      this.generated = false;
+      let self = this;
+      // viene usato setTimeout per simulare un caricamento
+      setTimeout(function(){
+        self.emailList = [];
+        for(let i=0; i<quantity; i++){
+          axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then(resp=>{
+              self.emailList.push(resp.data.response);
+            })
+        }
+        self.generated = true;
+      },1000);
+    }
+  },
+  mounted(){
+    this.generateEmailList(10);
+  }
+});
