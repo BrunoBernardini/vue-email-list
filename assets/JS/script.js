b5ae7471522e517cb0a0034a42506355b3cc6e14
BrunoBernardini <bruno.f.bernardini@gmail.com>
2022-05-12 10:10:15 +0200

    Aggiunta gestione degli errori.

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -11,24 +11,31 @@ Far comparire gli indirizzi email solamente quando sono stati tutti generati.
 const app = new Vue({
   el: "#app",
   data: {
+    totalEmails: 10,
     emailList: [],
-    generated: false
+    generated: false,
+    errorMsg: "",
+    isError: false,
   },
   methods: {
-    generateEmailList(quantity){
+    generateEmailList(){
       // La variabile "generated" viene comunque inizializzata a false nel caso in cui la funzione venisse richiamata altre volte tramite il pulsante.
       this.generated = false;
       this.emailList = [];
-      for(let i=0; i<quantity; i++){
+      for(let i=0; i<this.totalEmails; i++){
         axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
           .then(resp=>{
             this.emailList.push(resp.data.response);
-            if(this.emailList.length === quantity) this.generated = true;
-          });
+            if(this.emailList.length === this.totalEmails) this.generated = true;
+          })
+          .catch(error=>{
+            this.errorMsg = error;
+            this.isError = true;
+          })
       }
     }
   },
   mounted(){
-    this.generateEmailList(10);
+    this.generateEmailList();
   }
 });
